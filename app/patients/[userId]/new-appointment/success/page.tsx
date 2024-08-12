a85ef6cbbab5832805b5ffee6299787e576f9703
crazyhaller <suvigyamadrid@gmail.com>
2024-08-12 20:48:56 +0530

    ADD: Success Page

---
+++ app/patients/[userId]/new-appointment/success/page.tsx
@@ -0,0 +1,42 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+
+const Success = () => {
+  return (
+    <div className="flex h-screen max-h-screen px-[5%]">
+      <div className="success-img">
+        <Link href="/">
+          <Image
+            src="/assets/icons/logo-full.svg"
+            alt="logo"
+            width={1000}
+            height={1000}
+            className="h-10 w-fit"
+          />
+        </Link>
+
+        <section className="flex flex-col items-center">
+          <Image
+            src="/assets/gifs/success.gif"
+            height={300}
+            width={280}
+            alt="success"
+          />
+          <h2 className="header mb-6 max-w-[600px] text-center">
+            Your <span className="text-green-500">Appointment Request</span> has
+            been successfully submitted.
+          </h2>
+          <p>We will be in touch shortly.</p>
+        </section>
+
+        <section className="request-details">
+          <p>Requested Appointment Details:</p>
+          <div className="flex items-center gap-3"></div>
+        </section>
+      </div>
+    </div>
+  )
+}
+
+export default Success
